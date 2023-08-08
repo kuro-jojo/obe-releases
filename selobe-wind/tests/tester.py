637af4a15e1d8bd985c04c711a45a603c2c5938d
keptn <keptn@keptn.sh>
2023-08-08 13:12:02 +0000

    Updated resource

---
+++ selobe-wind/tests/tester.py
@@ -0,0 +1,8 @@
+import logging, time
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
+    logging.info("Running multiple tests...")
+    time.sleep(10)
+    logging.info("Finished running multiple tests.")
